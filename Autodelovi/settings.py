0f34c8d40d7ffb5fc77c5c882e3e086b5d72fd52
Stevan Meandzija <stevan.meandzija@gmail.com>
2022-12-05 23:36:34 +0100

    sad

--- Autodelovi/settings.py
+++ Autodelovi/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['autodelovishop.rs']
+CSRF_TRUSTED_ORIGINS = ['https://autodelovishop.rs']
 
 
 # Application definition
