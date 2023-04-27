d29fd746c39fd30e3ffa8e438c4d27e99effe268
Branko <olujic.branko@gmail.com>
2023-04-27 23:43:34 +0200

    cron

--- commands/cron.sh
+++ commands/cron.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/www/AutodeloviShop
 source venv/bin/activate
+cd commands
 
-python /var/www/AutodeloviShop/commands/stock.py
-python /var/www/AutodeloviShop/commands/populate.py
-python /var/www/AutodeloviShop/commands/images.py
-0 2 * * * source /var/www/AutodeloviShop/commands/cron.sh
+python stock.py
+python populate.py
+python images.py
