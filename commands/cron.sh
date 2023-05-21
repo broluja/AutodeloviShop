e53f1d627bab1a979e38c847525937a47f6ef80c
Branko <olujic.branko@gmail.com>
2023-05-21 21:51:20 +0200

    cron

--- commands/cron.sh
+++ commands/cron.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+rm GENUINE_02850.txt OUTOFSTOCK_SRB.txt PRICELIST_02850.txt REFAR_02850.txt
+unzip /home/autodelovi/ftp/OUTOFSTOCK_SRB.ZIP -A
+
+unzip /home/autodelovi/ftp/PRICELIST_02850.ZIP
+
+unzip /home/autodelovi/ftp/GENUINE_02850.ZIP
+
+unzip /home/autodelovi/ftp/REFAR_02850.ZIP
+
 cd /var/www/AutodeloviShop
 source venv/bin/activate
 cd commands
