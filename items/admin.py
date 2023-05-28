94397c0c2cd47772609ff7f6229690e415ec3868
Branko <olujic.branko@gmail.com>
2023-05-28 18:48:29 +0200

    adding link to the Item page in admin panel

--- items/admin.py
+++ items/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 from .models import Item, Brand
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "gbg_id", "views", "orders"]
+    list_display = ["__str__", "views", "orders", "open_item"]
     list_per_page = 20
-    list_display_links = ["__str__", "gbg_id"]
+    list_display_links = ["__str__", ]
+
+    def open_item(self, obj):
+        return mark_safe(  # Creating link to the Item page
+            f'<a target="_blank" href="{reverse("item:product_details", args=(obj.gbg_id,))}">product: {obj.gbg_id}</a>'
+        )
+
+    open_item.short_description = 'visit item'
 
 
 @admin.register(Brand)
